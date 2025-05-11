7a2973af887aadc59ccc5de2181c19dac9169f62
eduardPaez2591 <74082205+eduardPaez2591@users.noreply.github.com>
2025-05-11 23:23:35 +0000

    configuracion dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bigdata",
+    author="Eduard Andres Paez Bohorquez",
+    author_email="eduard.paez@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "yf",
+        "sqlite3"
+    ]
+)
